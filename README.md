30645b47834941bd806455489f9d3fd8a7e626fd
Yuki-At <yuki.yuki.yoh@gmail.com>
2021-02-07 12:00:58 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# SwapPlugin
+
+## Overview
+何分かおきに誰かと誰かの場所が交代するプラグイン。
+
+## Remarks
+一定時間ごとに被りなしでランダムにスワップ。tpされる10秒前からチャットにカウントのメッセージが入る。tp先の人を表示する。
+
+## Requirements
+Minecraft 1.15.2, PaperMC 1.15.2
---
+++ src/main/java/net/kunmc/lab/swapplugin/SwapPlugin.java
@@ -0,0 +1,14 @@
+package net.kunmc.lab.swapplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SwapPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
