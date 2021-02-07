bd9ef9333b720b136622ecdec340aca6b883d497
Yuki-At <yuki.yuki.yoh@gmail.com>
2021-02-07 18:05:40 +0900

    完成！

--- src/main/java/net/kunmc/lab/swapplugin/commands/SwapCommand.java
+++ src/main/java/net/kunmc/lab/swapplugin/commands/SwapCommand.java
@@ -92,8 +92,6 @@ public class SwapCommand implements CommandExecutor, TabCompleter {
 
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
-        System.out.println(args.length);
-
         if (args.length == 1) {
             return Arrays.asList("start", "interval", "end");
         }
